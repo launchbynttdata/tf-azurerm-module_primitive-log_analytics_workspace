9c4fe7df0b345d1c7115fdaa690534650bb837bc
nexient-ajoshi <97111743+nexient-ajoshi@users.noreply.github.com>
2024-03-07 11:15:18 -0500

    Initial module (#1)
    Co-authored-by: Aarti Joshi <aarti.joshi@nttdata.com>

---
+++ examples/log_analytics_workspace/test.tfvars
@@ -0,0 +1,21 @@
+logical_product_service = "hublogs"
+logical_product_family  = "launch"
+class_env               = "sandbox"
+instance_env            = 0
+instance_resource       = 0
+location                = "eastus"
+sku                     = "PerGB2018"
+retention_in_days       = 30
+resource_names_map = {
+  resource_group = {
+    name       = "rg"
+    max_length = 80
+  }
+  log_analytics_workspace = {
+    name       = "law"
+    max_length = 80
+  }
+}
+identity = {
+  type = "SystemAssigned"
+}
---
+++ variables.tf
@@ -0,0 +1,59 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+variable "name" {
+  type        = string
+  description = "(Required) Specifies the name of the Log Analytics Workspace. Workspace name should include 4-63 letters, digits or '-'. The '-' shouldn't be the first or the last symbol. Changing this forces a new resource to be created."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "(Required) The name of the resource group in which the Log Analytics workspace is created. Changing this forces a new resource to be created."
+}
+
+variable "location" {
+  type        = string
+  description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
+}
+
+variable "sku" {
+  type        = string
+  description = "(Optional) Specifies the SKU of the Log Analytics Workspace. Possible values are Free, PerNode, Premium, Standard, Standalone, Unlimited, CapacityReservation, and PerGB2018 (new SKU as of 2018-04-03). Defaults to PerGB2018."
+  default     = "Free"
+}
+
+variable "retention_in_days" {
+  type        = number
+  description = "(Optional) The workspace data retention in days. Possible values are either 7 (Free Tier only) or range between 30 and 730."
+  default     = "30"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "(Optional) A mapping of tags to assign to the resource."
+  default     = {}
+}
+
+variable "identity" {
+  type = object({
+    type         = string
+    identity_ids = optional(list(string))
+  })
+  description = "(Optional) A identity block as defined below."
+  default     = null
+}
+
+variable "local_authentication_disabled" {
+  type        = bool
+  description = "(Optional) Boolean flag to specify whether local authentication should be disabled. Defaults to false."
+  default     = false
+}
