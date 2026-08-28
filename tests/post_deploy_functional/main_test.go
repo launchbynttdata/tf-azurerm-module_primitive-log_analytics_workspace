f4279703d08191dc202db9e7bec4b8ece0c2725f
Ben Vaughan <ben.vaughan@nttdata.com>
2026-08-28 14:05:15 -0500

    fix: consolidate Dependabot updates and Terratest 1.x

--- tests/post_deploy_functional/main_test.go
+++ tests/post_deploy_functional/main_test.go
@@ -38,5 +38,5 @@ func TestLogAnalyticsModule(t *testing.T) {
 		}).
 		Build()
 
-	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestLogAnalyticsWorkspace)
+	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestComposableLogAnalyticsWorkspace)
 }
